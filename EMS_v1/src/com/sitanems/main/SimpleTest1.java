1dbb7967b1992fd1ff1d100b1077d9db408b44ca
wupansong <bbsctor@126.com>
2016-11-17 11:30:12 +0800

    引入spring框架，通过依赖注入的方式初始化modbus配置文件解析器，配置项对象，协议上下文对象，设备对象，增加了根据不同格式配置文件设定相关处理类的灵活性。

--- EMS_v1/src/com/sitanems/main/SimpleTest1.java
+++ EMS_v1/src/com/sitanems/main/SimpleTest1.java
@@ -1,13 +1,19 @@
 package com.sitanems.main;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import com.sitanems.device.commonDevice.ModbusTcpDevice;
 import com.sitanems.device.util.ModbusTcpDeviceFactory;
 
 public class SimpleTest1 {
 
 	public static void main(String[] args) {
-		ModbusTcpDevice device = ModbusTcpDeviceFactory.getDevice("src/com/sitanems/modbus/inPower/InPowerModbus.cfg");
+		//ModbusTcpDevice device = ModbusTcpDeviceFactory.getDevice("src/com/sitanems/modbus/inPower/InPowerModbus.cfg");
 
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml"); 
+		ModbusTcpDevice device = (ModbusTcpDevice) ctx.getBean("inPowerDevice");
+		
 		if (device != null)
 		{
 			device.execute( "485_3’æµÿ÷∑(IOÕ®—∂∞Â)", "W");
